f6f0237353ba3373521a4080e60ca3bf71ffe714
unknown <anneshgurav.xworkz@gmail.com>
2023-05-03 22:53:43 +0530

    JDBC

--- JDBC.sql
+++ JDBC.sql
@@ -3,7 +3,11 @@ CREATE DATABASE jdbc;
 SELECT * FROM moviedetails;
 
 SELECT * FROM profile;
+
 SELECT * FROM icecreamdetails;
+
 SELECT * FROM laptopdetails;
+
 SELECT * FROM osdetails;
+
 SELECT * FROM softwaredetails;
